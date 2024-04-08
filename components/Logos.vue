68b43b1c74954e4fc7e3f566fc5d599789f26800
Roger Killer <roger.killer@advery.ch>
2024-04-08 21:10:35 +0200

    Add Kirby logo SVG file and update Kirby component to use the SVG file as an image

--- components/Logos.vue
+++ components/Logos.vue
@@ -4,33 +4,33 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="-mx-6 grid grid-cols-1 sm:grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-3">
         <div class="bg-white/5 p-8 sm:p-10">
-          <NuxtLink to="https://nuxt.com/" target="_blank" aria-label="Visit Nuxtjs" class="max-h-12 w-full object-contain">
-            <LogoNuxt />
+          <NuxtLink to="https://nuxt.com/" target="_blank" aria-label="Visit Nuxtjs" class="w-full object-contain inline-flex justify-center">
+            <LogoNuxt class="max-w-24"/>
           </NuxtLink>
         </div>
         <div class="bg-white/5 p-6 sm:p-10 inline-flex items-center">
-          <NuxtLink to="https://tailwindcss.com/" target="_blank" aria-label="Visit Tailwind CSS" class="max-h-12 w-full object-contain">
-            <LogoTailwind />
+          <NuxtLink to="https://tailwindcss.com/" target="_blank" aria-label="Visit Tailwind CSS" class="w-full object-contain inline-flex justify-center">
+            <LogoTailwind class="max-w-24" />
           </NuxtLink>
         </div>
         <div class="bg-white/5 p-6 sm:p-10">
-          <NuxtLink to="https://laravel.com/" target="_blank" aria-label="Visit Kirby cms" class="max-h-12 w-full object-contain">
-            <LogoKirby />
+          <NuxtLink to="https://laravel.com/" target="_blank" aria-label="Visit Kirby cms" class="w-full object-contain inline-flex justify-center">
+            <LogoKirby class="max-w-24" />
           </NuxtLink>
         </div>
         <div class="bg-white/5 p-6 sm:p-10">
-          <NuxtLink to="https://strapi.io/" target="_blank" aria-label="Visit Strapi.io Headless CMS" class="max-h-12 w-full object-contain">
-            <LogoStrapi />
+          <NuxtLink to="https://strapi.io/" target="_blank" aria-label="Visit Strapi.io Headless CMS" class="w-full object-contain inline-flex justify-center">
+            <LogoStrapi class="max-w-24"/>
           </NuxtLink>
         </div>
         <div class="bg-white/5 p-6 sm:p-10">
-          <NuxtLink to="https://nextjs.org/" target="_blank" aria-label="Visit Nextjs" class="max-h-12 w-full object-contain">
-            <LogoNextjs />
+          <NuxtLink to="https://nextjs.org/" target="_blank" aria-label="Visit Nextjs" class="w-full object-contain inline-flex justify-center">
+            <LogoNextjs class="max-w-24" />
           </NuxtLink>
         </div>
         <div class="bg-white/5 p-6 sm:p-10">
-          <NuxtLink to="https://supabase.com/" target="_blank" aria-label="Visit Supabase" class="max-h-12 w-full object-contain">
-            <LogoSupabase />
+          <NuxtLink to="https://supabase.com/" target="_blank" aria-label="Visit Supabase" class="w-full object-contain inline-flex justify-center">
+            <LogoSupabase class="max-w-24"/>
           </NuxtLink>
         </div>
       </div>
