a5316b03c22fd407a079fa5601baea254dbabc64
Roger Killer <roger.killer@advery.ch>
2024-05-09 19:58:46 +0200

    ✨ (blog/[...slug].vue): improve styling of back button by adding max-w-max class to limit width to content size

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -11,7 +11,7 @@ const { data: blogPost } = await useAsyncData(`content-${path}`, () => {
     <BaseSection>
       <BaseContainer>
         <!-- create a back button -->
-        <NuxtLink to="/" class="text-primary-400 mb-8 block">Back</NuxtLink>
+        <NuxtLink to="/" class="text-primary-400 mb-8 block max-w-max">Back</NuxtLink>
         <ContentDoc class="prose text-white prose-headings:text-white prose-strong:text-primary-400 prose-a:text-white prose-p:font-serif prose-li:font-serif prose-strong:font-serif"/>
       </BaseContainer>
     </BaseSection>
