8d75f8acb38bf5a68aad9871469e11ad7c6aba89
Roger Killer <roger.killer@advery.ch>
2024-05-09 19:54:41 +0200

    ✨ (app.vue): add page transition styles for smoother page transitions 🔧 (nuxt.config.ts): configure page transition settings for the Nuxt app to use 'page' transition with 'out-in' mode

--- app.vue
+++ app.vue
@@ -12,3 +12,14 @@ useHead({
   <NuxtLayout><NuxtPage /> </NuxtLayout>
 </template>
 
+<style>
+.page.enter-active, .page-leave-active {
+  transition: opacity 0.4s;
+}
+
+.page-enter-from, .page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
+
