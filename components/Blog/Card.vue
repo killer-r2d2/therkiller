7a0c5b51bc2d0b7340d56d948adbb0433dad65fc
Roger Killer <roger.killer@advery.ch>
2024-08-18 13:41:17 +0200

    removed image from Card.vue

--- components/Blog/Card.vue
+++ components/Blog/Card.vue
@@ -23,19 +23,6 @@ const formatDate = (date: string) => {
   >
     <div class="p-4 flex flex-col flex-grow">
       <div
-        v-if="props.blogPost.image"
-        class="relative pb-[60%] rounded-2xl mb-2"
-      >
-        <NuxtImg
-          :src="props.blogPost.image"
-          :alt="props.blogPost.title"
-          class="absolute top-0 left-0 w-full h-full object-cover rounded-2xl"
-          loading="lazy"
-          quality="65"
-          format="webp"
-        />
-      </div>
-      <div
         class="group-hover:opacity-80 transition-all ease-in-out duration-75 flex-grow"
       >
         <p class="text-sm mb-2">
