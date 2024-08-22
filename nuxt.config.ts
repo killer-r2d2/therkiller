aa7aac1e130970a682cb181250b96f915102afcf
Roger Killer <roger.killer@advery.ch>
2024-08-22 21:55:17 +0200

    ✨ (nuxt.config.ts): add nitro static configuration for improved performance ✨ (nuxt.config.ts): add routeRules for prerendering specific routes to enhance SEO and performance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    static: true,
+  },
+  routeRules: {
+    '/': { prerender: true }, // Pre-renders the home page
+    '/blog/**': { prerender: true }, // Pre-renders all blog pages
+  },
   app: {  
     pageTransition: {
       name: 'page',
