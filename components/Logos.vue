e07b1c77deef81bb350e24da4875aed1f222fcb0
Roger Killer <killer@gmail.com>
2025-02-18 20:55:12 +0100

    style(Logos.vue): update class structure for NuxtLink components to improve layout consistency and readability

--- components/Logos.vue
+++ components/Logos.vue
@@ -3,68 +3,73 @@
   <BaseSection>
     <BaseContainer>
         <div
-          class=" grid grid-cols-1 sm:grid-cols-2 gap-5 overflow-hidden sm:mx-0 md:grid-cols-3"
+          class="grid grid-cols-1 sm:grid-cols-2 gap-5 overflow-hidden sm:mx-0 md:grid-cols-3 p-5"
         >
-          <div class="p-4 border-2 border-white rounded-lg flex justify-center items-center ">
-            <NuxtLink
-              to="https://nuxt.com/"
-              target="_blank"
-              aria-label="Visit Nuxtjs"
-              class="w-full object-contain inline-flex justify-center transform scale-100 transition-all hover:scale-110 duration-300"
-            >
+          <NuxtLink
+            to="https://nuxt.com/"
+            target="_blank"
+            aria-label="Visit Nuxtjs"
+            class="transform scale-100 transition-all hover:scale-110 duration-300"
+          >
+            <div class="p-4 h-full border-2 border-white rounded-lg flex justify-center items-center">
               <LogoNuxt class="max-w-24" />
-            </NuxtLink>
-          </div>
-          <div class="p-4 border-2 border-white rounded-lg flex justify-center items-center">
-            <NuxtLink
-              to="https://tailwindcss.com/"
-              target="_blank"
-              aria-label="Visit Tailwind CSS"
-              class="w-full object-contain inline-flex justify-center transform scale-100 transition-all hover:scale-110 duration-300"
-            >
+            </div>
+          </NuxtLink>
+          
+          <NuxtLink
+            to="https://tailwindcss.com/"
+            target="_blank"
+            aria-label="Visit Tailwind CSS"
+            class="transform scale-100 transition-all hover:scale-110 duration-300"
+          >
+            <div class="p-4 h-full border-2 border-white rounded-lg flex justify-center items-center">
               <LogoTailwind class="max-w-24" />
-            </NuxtLink>
-          </div>
-          <div class="p-4 border-2 border-white rounded-lg flex justify-center items-center">
-            <NuxtLink
-              to="https://getkirby.com//"
-              target="_blank"
-              aria-label="Visit Kirby cms"
-              class="w-full object-contain inline-flex justify-center transform scale-100 transition-all hover:scale-110 duration-300"
-            >
+            </div>
+          </NuxtLink>
+
+          <NuxtLink
+            to="https://getkirby.com//"
+            target="_blank"
+            aria-label="Visit Kirby cms"
+            class="transform scale-100 transition-all hover:scale-110 duration-300"
+          >
+            <div class="p-4 h-full border-2 border-white rounded-lg flex justify-center items-center">
               <LogoKirby class="max-w-24" />
-            </NuxtLink>
-          </div>
-          <div class="p-4 border-2 border-white rounded-lg flex justify-center items-center">
-            <NuxtLink
-              to="https://strapi.io/"
-              target="_blank"
-              aria-label="Visit Strapi.io Headless CMS"
-              class="w-full object-contain inline-flex justify-center transform scale-100 transition-all hover:scale-110 duration-300"
-            >
+            </div>
+          </NuxtLink>
+
+          <NuxtLink
+            to="https://strapi.io/"
+            target="_blank"
+            aria-label="Visit Strapi.io Headless CMS"
+            class="transform scale-100 transition-all hover:scale-110 duration-300"
+          >
+            <div class="p-4 h-full border-2 border-white rounded-lg flex justify-center items-center">
               <LogoStrapi class="max-w-24" />
-            </NuxtLink>
-          </div>
-          <div class="p-4 border-2 border-white rounded-lg flex justify-center items-center">
-            <NuxtLink
-              to="https://nextjs.org/"
-              target="_blank"
-              aria-label="Visit Nextjs"
-              class="w-full object-contain inline-flex justify-center transform scale-100 transition-all hover:scale-110 duration-300"
-            >
+            </div>
+          </NuxtLink>
+
+          <NuxtLink
+            to="https://nextjs.org/"
+            target="_blank"
+            aria-label="Visit Nextjs"
+            class="transform scale-100 transition-all hover:scale-110 duration-300"
+          >
+            <div class="p-4 h-full border-2 border-white rounded-lg flex justify-center items-center">
               <LogoNextjs class="max-w-24" />
-            </NuxtLink>
-          </div>
-          <div class="p-4 border-2 border-white rounded-lg flex justify-center items-center">
-            <NuxtLink
-              to="https://supabase.com/"
-              target="_blank"
-              aria-label="Visit Supabase"
-              class="w-full object-contain inline-flex justify-center transform scale-100 transition-all hover:scale-110 duration-300"
-            >
+            </div>
+          </NuxtLink>
+
+          <NuxtLink
+            to="https://supabase.com/"
+            target="_blank"
+            aria-label="Visit Supabase"
+            class="transform scale-100 transition-all hover:scale-110 duration-300"
+          >
+            <div class="p-4 h-full border-2 border-white rounded-lg flex justify-center items-center">
               <LogoSupabase class="max-w-24" />
-            </NuxtLink>
-          </div>
+            </div>
+          </NuxtLink>
         </div>
     </BaseContainer>
   </BaseSection>
