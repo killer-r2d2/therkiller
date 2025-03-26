7f72a5b324564d9553b32275ae2886ffac575339
Roger Killer <killerroger4@gmail.com>
2025-03-26 15:18:21 +0100

    refactor(blog slug): remove unnecessary script setup and async data handling for cleaner code and improved readability

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-import { useRoute } from 'vue-router';
-const { $blogRepository } = useNuxtApp();
-const route = useRoute();
-
-const { data: blogPost, error } = await useAsyncData(`content-${route.path}`, () => {
-  return $blogRepository.getPostByPath(route.path);
-});
-</script>
-
 <template>
   <main>
     <BaseSection>
@@ -16,9 +6,7 @@ const { data: blogPost, error } = await useAsyncData(`content-${route.path}`, ()
         <NuxtLink to="/" class="text-primary-400 mb-8 block max-w-max">Back</NuxtLink>
 
         <!-- Display the content only if the blogPost is available -->
-        <div v-if="pending">Loading...</div>
-        <div v-else-if="error">Error loading post: {{ error.message }}</div>
-        <ContentDoc v-else class="prose text-white prose-headings:text-white prose-strong:text-primary-400 prose-a:text-white prose-p:font-serif prose-li:font-serif prose-strong:font-serif" />
+        <ContentDoc class="prose text-white prose-headings:text-white prose-strong:text-primary-400 prose-a:text-white prose-p:font-serif prose-li:font-serif prose-strong:font-serif" />
       </BaseContainer>
     </BaseSection>
   </main>
