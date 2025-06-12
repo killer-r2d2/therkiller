d8c8e4dc762b3bc1176393dbaf4f874d946c12cd
Roger Killer <killerroger4@gmail.com>
2025-06-12 09:29:20 +0200

    chore: update package dependencies to include @nuxtjs/supabase and @vercel/speed-insights, and modify default layout to integrate SpeedInsights component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,12 @@
+<script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
 <template>
 	<div class="bg-black flex min-h-screen flex-col text-white">
 		<div class="grow">
 			<NuxtPage />
 		</div>
+		<SpeedInsights />
 	</div>
 </template>
