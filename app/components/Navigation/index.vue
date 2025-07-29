9d67bbe906899b02a139bdd566e6b2cd9b38ef97
Roger Killer <killerroger4@gmail.com>
2025-07-29 14:26:52 +0200

    style: enhance navigation component with consistent margin settings for improved layout

--- app/components/Navigation/index.vue
+++ app/components/Navigation/index.vue
@@ -3,19 +3,18 @@
 </script>
 
 <template>
-  <nav>
-    <BaseSection>
-      <BaseContainer class="flex justify-center gap-x-12">
-        <NuxtLink to="/" class="flex items-center text-right text-primary-400 hover:text-primary-300 transition-colors duration-200">
-          <span class="sr-only">Go to Home Page</span>
-          <Icon name="lucide:home" size="26" />
-        </NuxtLink>
-        <NuxtLink to="/login"
-          class="flex items-center text-right text-primary-400 hover:text-primary-300 transition-colors duration-200">
-          <span class="sr-only">Go to Login Page</span>
-          <Icon name="lucide:circle-user" size="26" />    
-        </NuxtLink>
-      </BaseContainer>
-    </BaseSection>
+  <nav class="my-12 md:my-12 lg:my-24">
+    <BaseContainer class="flex justify-center gap-x-12">
+      <NuxtLink to="/"
+        class="flex items-center text-right text-primary-400 hover:text-primary-300 transition-colors duration-200">
+        <span class="sr-only">Go to Home Page</span>
+        <Icon name="lucide:home" size="26" />
+      </NuxtLink>
+      <NuxtLink to="/login"
+        class="flex items-center text-right text-primary-400 hover:text-primary-300 transition-colors duration-200">
+        <span class="sr-only">Go to Login Page</span>
+        <Icon name="lucide:circle-user" size="26" />
+      </NuxtLink>
+    </BaseContainer>
   </nav>
 </template>
