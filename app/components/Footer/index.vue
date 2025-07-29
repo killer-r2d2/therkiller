26c0803b642d043c4041c66d099b37e16194c3a0
Roger Killer <killerroger4@gmail.com>
2025-07-29 14:34:07 +0200

    style: refine footer layout by removing unnecessary BaseSection wrapper and adjusting margins for improved spacing

--- app/components/Footer/index.vue
+++ app/components/Footer/index.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-	<footer>
-		<BaseSection>
-			<BaseContainer>
-				<div class="flex flex-col items-center text-center">
-					<NuxtLink to="/" class="text-primary-400 text-sm mb-4">therkiller.dev</NuxtLink>
-					<div class="gap-x-4 flex flex-row">
-						<SocialLinks />
-					</div>
+	<footer class="mb-12 md:mb-24 lg:mb-44">
+		<BaseContainer>
+			<div class="flex flex-col items-center text-center">
+				<NuxtLink to="/" class="text-primary-400 text-sm mb-4">therkiller.dev</NuxtLink>
+				<div class="gap-x-4 flex flex-row">
+					<SocialLinks />
 				</div>
-			</BaseContainer>
-		</BaseSection>
+			</div>
+		</BaseContainer>
 	</footer>
 </template>
