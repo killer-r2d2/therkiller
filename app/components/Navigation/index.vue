84baf697f659599471f0ada32dd4f87e46645543
Roger Killer <killerroger4@gmail.com>
2026-03-28 20:12:39 +0100

    refactor: simplify Navigation component structure and update link text to 'therkiller.dev'

--- app/components/Navigation/index.vue
+++ app/components/Navigation/index.vue
@@ -1,18 +1,14 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <nav class="my-12 md:my-12 lg:my-24 ">
-    <div class="flex justify-end container mx-auto">
-      <div class="border-2 border-primary-400/20 bg-primary-400/20 backdrop-blur-[1px] max-w-max flex items-center justify-center gap-x-4 rounded-full p-2">
-        <NuxtLink
-to="/"
-          class="flex items-center justify-center w-[26px] h-[26px] text-primary-400 hover:text-primary-300 transition-colors duration-200">
-          <span class="sr-only">Go to Home Page</span>
-          <Icon name="lucide:home" size="26" />
-        </NuxtLink>
-      </div>
-    </div>
-  </nav>
+	<nav
+		class="container mx-auto my-12 flex items-center justify-between md:my-12 lg:my-24"
+	>
+		<NuxtLink
+			to="/"
+			class="text-primary-400 hover:text-primary-300 h-[26px] w-[26px] items-center justify-center transition-colors duration-200"
+		>
+			therkiller.dev
+		</NuxtLink>
+	</nav>
 </template>
