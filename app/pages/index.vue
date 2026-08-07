c2406fcabd59c48eff27a41d3e786edf7a2b69c9
Roger Killer <killerroger4@gmail.com>
2026-08-07 21:09:25 +0200

    fix: show blog before projects

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,16 +2,16 @@
 	<main>
 		<TheHero />
 		<div>
-			<LazyProjectList
-				heading="Selected projects"
-				description="Recent websites built with Kirby CMS, Nuxt, Strapi, and Tailwind CSS."
-				:limit="3"
-				show-all-link
-			/>
 			<LazyBlogPostList
 				heading="Latest blog posts"
 				description="A selection of my latest notes on web development, tooling, and the things I learn along the way."
 				:limit="5"
+				show-all-link
+			/>
+			<LazyProjectList
+				heading="Selected projects"
+				description="Recent websites built with Kirby CMS, Nuxt, Strapi, and Tailwind CSS."
+				:limit="3"
 				show-all-link
 			/>
 			<LazyLogoCollection />
